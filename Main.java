673d2d9f365952fa281a3f4a5133f0fe753ff85c
endyDev <endyten1621@gmail.com>
2025-12-30 00:39:52 +0300

    Refactor Main class and add Song instances

--- Main.java
+++ Main.java
@@ -1,11 +1,34 @@
-package com.endyspotify;
+package com.EndieFirstProje;
+
+import com.EndieFirstProje;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        
+        
+      Song s1 = new Song ("loading","central cee","can't rush greatness","trap");
+        
+        Song s2 = new Song ("no introduction ","central cee","can't rush greatness","trap");
+        
+        Song s3 = new Song ("overseas","central cee","can't rush greatness","trap");
+        
+        
+        Song s4 = new Song ("must be ","central cee","can't rush greatness","trap");
+        
+        
+        
+        s1.displayInfo();
+          s2.displayInfo();
+                s3.displayInfo();
+          s4.displayInfo();
+                
+        
+        
+  
+        
     }
-}
+        
+    }
+    
+    
+
